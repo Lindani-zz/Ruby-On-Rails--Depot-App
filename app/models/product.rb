f30ed708c60a6bd098880ef94458b96537febae4
lindani <lindani@projectcodex.co>
2016-02-23 11:59:40 +0200

    error message updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	validates :description, :image_url, presence: true
-	validates :title, presence: true, length: { minimum: 10, message: "can't be blank"}
+	validates :title, presence: true, length: { minimum: 10, message: "can't be blank with minimum of 10 characters"}
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :image_url, allow_blank: true, format: {
 	with: %r{\.(gif|jpg|png)\Z}i,
