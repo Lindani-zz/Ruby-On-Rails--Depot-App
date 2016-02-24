4a0e5e3229600e1caadc703b23c2a5d3fc151858
lindani <lindani@projectcodex.co>
2016-02-24 14:39:21 +0200

    Adde test to check if product instance was assigned

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,6 +4,8 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:products)
+    # assert_template()
   end
 
 end
