57c08628971beda5dabe3daf7d14f729ec96d98d
lindani <lindani@projectcodex.co>
2016-02-24 14:52:17 +0200

    added test for the correct layout and template

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,8 +4,9 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:products)
-    # assert_template()
+    assert_not_nil assigns :products
+    assert_template :index, message: "Wrong template"
+    assert_template layout: "application"
   end
 
 end
