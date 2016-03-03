bcd64b605c45d79ee8d7adea1ad3a7e8247d1811
lindani <lindani@projectcodex.co>
2016-03-03 14:15:37 +0200

    fix indentation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
 	has_many :line_items
-	 before_destroy :ensure_not_referenced_by_any_line_item
+	before_destroy :ensure_not_referenced_by_any_line_item
 	validates :description, :image_url, presence: true
 	validates :title, uniqueness: true, presence: true, length: { minimum: 10 }
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
