0d23886e108f7ea51e7ca8d680448a1f9b5e4c6b
lindani <lindani@projectcodex.co>
2016-03-03 14:32:10 +0200

    added helper method to be called in application views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def hidden_div_if(condition, attributes = {}, &block)
+		if condition
+			attributes["style"] = "display: none"
+		end
+		#Rails standard helper
+		content_tag("div", attributes, &block)
+	end
 end
