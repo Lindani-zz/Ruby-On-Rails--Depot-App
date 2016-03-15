485398042720d4efa1ed3a94d666c5a782ff80e9
lindani <lindani@projectcodex.co>
2016-03-15 17:35:46 +0200

    Add test to ensure there is an item in the cart in orders controller test

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -17,15 +17,15 @@ class OrdersControllerTest < ActionController::TestCase
     assert_equal flash[:notice], 'Your cart is empty'
   end
 
-  # test "should get new" do
-  #   item = LineItem.new
-  #   item.build_cart
-  #   item.product = products(:ruby)
-  #   item.save!
-  #   session[:cart_id] = item.cart.id
-  #   get :new
-  #   assert_response :success
-  # end
+  test "should get new" do
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
+    get :new
+    assert_response :success
+  end
 
   test "should create order" do
     assert_difference('Order.count') do
